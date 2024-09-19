e585c56c50d2142788c4c64c903d0d2d1900c3c6
tomh@dedalus <celephicus@gmail.com>
2024-09-20 09:39:13 +1000

    Note to self: finish Sentrybot. He's getting bored in the box on the shelf in the back room.

---
+++ sentrybot/gpio.h
@@ -0,0 +1,11 @@
+
+#ifndef GPIO_H__
+#define 
+
+constexpr uint8_t GPIO_PIN_MOVEMENT_SENSOR = D2;
+constexpr uint8_t GPIO_PIN_MOUTH_LED_RED = D3;
+constexpr uint8_t GPIO_PIN_MOUTH_LED_GREEN = D5;
+constexpr uint8_t GPIO_PIN_MOUTH_LED_BLUE = D6;
+
+
+#endif	// GPIO_H__
