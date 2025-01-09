b73ad1cdf16e10bd1f4abf64eb8dee6cfd5fd950
tomh@dedalus <celephicus@gmail.com>
2025-01-09 13:36:38 +1100

    Pending edits from dedalus.

--- table/demo/demo.ino
+++ table/demo/demo.ino
@@ -3,14 +3,14 @@
 FASTLED_USING_NAMESPACE
 
 // GPIO
-#define DATA_PIN    A0
+#define DATA_PIN    5
 constexpr uint8_t GPIO_BRIGHTNESS_POT = A1;
 constexpr uint8_t GPIO_BRIGHTNESS_VCC = A2;
 
 
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
-#define NUM_LEDS    22
+#define NUM_LEDS    245
 CRGB leds[NUM_LEDS];
 
 constexpr uint8_t BRIGHTNESS_MIN = 17;
@@ -24,7 +24,7 @@ void setup() {
   //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
   // set master brightness control
-  FastLED.setBrightness(11);
+  FastLED.setBrightness(255);
 
   pinMode(GPIO_BRIGHTNESS_VCC, 1);
   digitalWrite(GPIO_BRIGHTNESS_VCC, 1);
@@ -44,10 +44,11 @@ void loop() {
   EVERY_N_MILLISECONDS( 20 ) {
     gHue++;  // slowly cycle the "base color" through the rainbow
   }
-
+/*
   EVERY_N_MILLISECONDS(100) {
     uint8_t b = (map(analogRead(GPIO_BRIGHTNESS_POT), 0, 1023, BRIGHTNESS_MIN, 255));
     Serial.println(b);
     FastLED.setBrightness(dim8_video(b));
   }
+  */
 }
